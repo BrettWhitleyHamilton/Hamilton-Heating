f8effc3674cca4d48e2fb501d32749779393c0c8
BrettWhitleyHamilton <bretth73@gmail.com>
2017-03-30 20:11:44 +0100

    V2 has form,styling is better

--- php/email.php
+++ php/email.php
@@ -2,18 +2,14 @@
 $name=$_POST['name'];
 $visitor_email=$_POST['email'];
 $message=$_POST['message'];
-$email_from = 'bretth73@gmail.com';
-$email_subject = "Website Form submission";
+$email_from = 'contact@hamiltonheating.co.uk';
+$email_subject = "Recieved message from $name";
 $email_body = "You have received a new message from $name.\n".
 "Here is the message:\n $message .";
-$to = "bretth73@gmail.com";
+$to = "hamiltonheating46@gmail.com";
+ $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
-if(@mail($emailRecipient, $subject, $message, $headers))
-{
-  echo "Mail Sent Successfully";
-}else{
-  echo "Mail Not Sent";
-}
 ?>
